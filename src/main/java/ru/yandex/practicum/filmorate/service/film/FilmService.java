daacb954ad21bf9887925be262a6c682690eaba2
Denis Krasko <iidonz143@gmail.com>
2026-07-31 06:02:55 +0300

    Реализовать функциональность Поиск

--- src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/film/FilmService.java
@@ -148,4 +148,9 @@ public class FilmService {
         }
         return filmOptional.get();
     }
+
+    public List<Film> searchFilms(String title) {
+        log.debug("Searshing movies");
+        return filmStorage.searchFilms(title);
+    }
 }
